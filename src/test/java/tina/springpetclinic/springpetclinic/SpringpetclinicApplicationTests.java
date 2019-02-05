b5831123af190070f7baf12a6c1782980f8f371e
Phaustina Namsenda <pnapunyi@gmail.com>
2019-02-05 10:23:05 +0300

    Initial Commit

---
+++ src/test/java/tina/springpetclinic/springpetclinic/SpringpetclinicApplicationTests.java
@@ -0,0 +1,17 @@
+package tina.springpetclinic.springpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringpetclinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
